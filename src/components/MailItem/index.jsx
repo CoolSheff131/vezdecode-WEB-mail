d7bcd19a9fa634a0d4af34db37931aef058e34a2
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-07-10 11:37:00 +0800

    item click fix

--- src/components/MailItem/index.jsx
+++ src/components/MailItem/index.jsx
@@ -3,27 +3,31 @@ import './styles/MailItem.css';
 
 const MailItem = ({ style, data, onCheckBoxClick, checkBoxValue, setMailToShow }) => {
   const checkBoxClickHandle = (event) => {
+    console.log('====================================');
+    console.log(event);
+    console.log('====================================');
     event.stopPropagation();
+    event.nativeEvent.stopImmediatePropagation();
     onCheckBoxClick();
   };
   return (
-    <div
-      style={style}
-      className="mail-item"
-      onClick={() => {
-        setMailToShow(data);
-      }}>
+    <div style={style} className="mail-item">
       <Checkbox checked={checkBoxValue} onClick={checkBoxClickHandle}></Checkbox>
+      <div
+        className="mail-item__data"
+        onClick={() => {
+          setMailToShow(data);
+        }}>
+        <div className="mail-item__author">{data.author.name}</div>
+        <div className="mail-item__title">{data.title}</div>
+        <div className="mail-item__text">{data.text}</div>
+        {data.finance && <div className="mail-item__finance">₽</div>}
+        <div className="mail-item__date-time">{data.dateTime}</div>
 
-      <div className="mail-item__author">{data.author.name}</div>
-      <div className="mail-item__title">{data.title}</div>
-      <div className="mail-item__text">{data.text}</div>
-      {data.finance && <div className="mail-item__finance">₽</div>}
-      <div className="mail-item__date-time">{data.dateTime}</div>
+        {!data.read && <div className="mail-item__new">New</div>}
 
-      {!data.read && <div className="mail-item__new">New</div>}
-
-      {data.important && <div className="mail-item__important">!</div>}
+        {data.important && <div className="mail-item__important">!</div>}
+      </div>
     </div>
   );
 };
